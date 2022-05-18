6f0dcf54d1e604209e47fff680558c212a1ae60d
shoval <shoval.weinstock@live.biu.ac.il>
2022-05-18 12:08:24 +0300

    applied search vid2

--- TikTalk/wwwroot/js/search.js
+++ TikTalk/wwwroot/js/search.js
@@ -1,8 +1,27 @@
 ﻿$(function () {
 
-    $('form').submit(function(e) {
+    $('form').submit(function (e) {
+
         e.preventDefault();
+
         const q = $('#search').val(); // getting the searched value
-        $('tbody').load('/Articles/Search3?query='+q); // filtering
-    })
+
+        var r = await fetch('/Articles/Search3?query=' + q);
+        //$('tbody').load('/Articles/Search3?query='+q); // filtering
+        var d = await r.jason();
+        Console.log(d); //todo delete
+
+
+        const template = $('#template').html();
+        let results = '';
+        for (var item in d) {
+            let row = template;
+            for (var key in d[item]) {
+                Console.log('{' + key + '}', d[item][key]); //todo delete
+                row = row.replaceAll('{' + key + '}', d[item][key]);
+            }
+            results += row;
+        }
+        $('tbody').html(results);
+    });
 });
